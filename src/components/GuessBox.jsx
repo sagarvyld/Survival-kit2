b7f0b5d5edcbc9f880b0a7e293b6591884bc6278
Sagar <sagar.kapoor@vyld.io>
2024-08-14 12:14:52 +0530

    done

--- src/components/GuessBox.jsx
+++ src/components/GuessBox.jsx
@@ -38,8 +38,10 @@ const GuessBox = ({
     }
   };
   const handleTextareaChange = (event) => {
-    const value = event.target.value.trim();
-    setword(value);
+    const value = event.target.value;
+    const emojiRegex = /[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{FE00}-\u{FE0F}]|[\u{1F000}-\u{1F02F}]|[\u{1F0A0}-\u{1F0FF}]|[\u{1F100}-\u{1F1FF}]|[\u{1F200}-\u{1F2FF}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F700}-\u{1F77F}]|[\u{1F780}-\u{1F7FF}]|[\u{1F800}-\u{1F8FF}]|[\u{1F900}-\u{1F9FF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1FA70}-\u{1FAFF}]|[\u{1FB00}-\u{1FBFF}]/gu;
+    const filteredValue = value.match(emojiRegex);
+    setword(filteredValue ? filteredValue.join('') : '');
     if (value === "") {
       setIsEmpty(true);
     } else {
@@ -87,6 +89,7 @@ const GuessBox = ({
       </div>
       <div className="guess_answer_survival">
         <textarea
+        value={word}
           ref={textareaRef}
           onChange={handleTextareaChange}
           onFocus={handleFocus}
